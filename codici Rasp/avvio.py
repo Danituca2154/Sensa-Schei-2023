512874fa1c836eec057e6fb157ef0e9cc788bb84
Danituca2154 <103452503+Danituca2154@users.noreply.github.com>
2023-06-30 15:27:17 +0200

    Add files via upload

---
+++ codici Rasp/avvio.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+import subprocess
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(23, GPIO.IN) 
+
+pulsante = 23
+
+commando1 = 'cd /home/sensaschei2/Desktop/codici_macc_nuova'
+
+commando2 = 'python3 LAMAPPADEFINITIVA.py'
+
+
+
+
+controllo = 0
+
+def button_callback(channel):
+	global controllo
+	print (controllo)
+	if (controllo == 0):
+		print( "START")
+		subprocess.call(commando1, shell=True)
+		sleep(0.1)
+		subprocess.call(commando2, shell=True)
+		controllo = 1
+		
+	
+GPIO.add_event_detect(pulsante, GPIO.FALLING, callback=button_callback, bouncetime = 100)
+
+while True:
+	pass
